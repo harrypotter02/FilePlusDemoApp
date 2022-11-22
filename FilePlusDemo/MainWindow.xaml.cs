61be91a6b275efb2e2b6e06af35e0bf221d92c23
02114 <02114@PC-02114>
2022-11-22 23:03:22 +0800

    First version of FilePlus demo

--- FilePlusDemo/MainWindow.xaml.cs
+++ FilePlusDemo/MainWindow.xaml.cs
@@ -3,6 +3,13 @@ using System.Windows;
 using System.Diagnostics;
 using System.ComponentModel;
 using FilePlusLibrary;
+using FilePlusDemo.SnackBarExample;
+
+using System.Collections.Generic;
+using MaterialDesignThemes.Wpf;
+using System.Linq;
+using static FilePlusDemo.SnackBarExample.MySnack;
+using FilePlusLibrary.OutputListboxExample;
 
 namespace FilePlusDemo
 {
@@ -12,6 +19,8 @@ namespace FilePlusDemo
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
         FilePlus flib;
+        CodeTime ctime;
+        private MySnack mysnack;
         private string _example1_str;
         public string Example1Str
         {
@@ -40,6 +49,8 @@ namespace FilePlusDemo
             }
         }
 
+        
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         public MainWindow()
@@ -47,12 +58,28 @@ namespace FilePlusDemo
             InitializeComponent();
             mymain_grid.DataContext = this;
             flib = new FilePlus();
+            ctime = new CodeTime();
+
             
+            
+            #region example6_MySnackBar
+            mysnack = new MySnack();
+            snap_example_grid.DataContext = mysnack;
+            snap_panel.DataContext = this;
+            MessageType = "MY_WARNING";
+            MyTalk = "Hello SnackBar With NORMAL/WARNING/ERROR level";
+            #endregion
+
+            #region example7_OutputListbox
+            MyInfoBox.SetRefWindowAndListBox(this,this.OutputListBox);
+            MyInfoBox.AddInfo("Easily show your text on listbox!");
+            MyInfoBox.AddInfo("this is next message");
+            #endregion
         }
 
         private void Window_Closed(object sender, EventArgs e)
         {
-            
+            flib.CloseDebug();//example0
         }
 
         //example1:
@@ -98,11 +125,11 @@ namespace FilePlusDemo
             Example2Str = "Hello write this message to log :)";
         }
 
-        private void create_dev_log_Click(object sender, RoutedEventArgs e)
+        private void create_dev_log_Click(object sender, RoutedEventArgs e)//example0
         {
-            flib.CreateDebug();
+            flib.CreateDebug(); 
             Debug.WriteLine("Hello Dev!");
-            flib.CloseDebug();
+            
             FinishedMsg();
         }
 
@@ -111,5 +138,94 @@ namespace FilePlusDemo
         {
             MessageBox.Show("Write message to file finished!");
         }
+
+        private void open_save_filedialog_Click(object sender, RoutedEventArgs e)
+        {
+            //flib.OpenSaveDialog("mysave.txt");
+            if(flib.OpenSaveDialog("", "ini (*.ini)|*.ini"))
+            {
+                int a = 99;
+            }
+            else
+            {
+                int b = 98;
+
+            }
+        }
+
+        private void open_file_filedialog_Click(object sender, RoutedEventArgs e)
+        {
+            flib.OpenFileDialog();
+        }
+
+        private void get_timestamp_Click(object sender, RoutedEventArgs e)
+        {
+
+            string time = ctime.GetTickMs().ToString();
+            MessageBox.Show("From Unix time 1970.. timestamp:"+time);
+        }
+
+        #region example6
+        private string _message_type;
+        public string MessageType
+        {
+            get
+            {
+                return _message_type;
+            }
+            set
+            {
+                _message_type = value;
+                NotifyPropertyChanged("MessageType");
+            }
+        }
+        private string _mytalk;
+        public string MyTalk
+        {
+            get
+            {
+                return _mytalk;
+            }
+            set
+            {
+                _mytalk = value;
+                NotifyPropertyChanged("MyTalk");
+            }
+        }
+        private void PopSnackBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageToSnackLevel level = MessageToSnackLevel.NoLevel;
+            if (MessageType == "MY_ERROR")
+                level = MessageToSnackLevel.Error;
+            else if (MessageType == "MY_WARNING")
+                level = MessageToSnackLevel.Warning;
+
+            mysnack.AddMessage(MyTalk,level,1);
+        }
+        #endregion
+        
+
+        #region example7
+        private bool _box_auto_scroll;
+        public bool BoxAutoScroll
+        {
+            set
+            {
+                _box_auto_scroll = value;
+                NotifyPropertyChanged("BoxAutoScroll");
+            }
+            get
+            {
+                return _box_auto_scroll;
+            }
+        }
+        private void CleanOutputButton_Click(object sender, RoutedEventArgs e)
+        {
+            MyInfoBox.Clean();
+        }
+
+        #endregion
+
+        
     }
 }
---
+++ FilePlusDemo/MySnackOptionConveter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace FilePlusDemo
+{
+    public class MySnackOptionConveter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string checkvalue = value.ToString();
+            string targetvalue = parameter.ToString();//我們設定給conveterParameter的字串
+            //變數value的字串 若與radiobox項目ConverterParameter字串相同,則設定為true
+            bool r = checkvalue.Equals(targetvalue, StringComparison.InvariantCultureIgnoreCase);
+            return r;
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //0: Linear robot
+            //1: dex robot
+            //value 是目前 radiobutton 的 true/false
+            //在這裡把 parameter 傳回 View-Model
+            if (value == null || parameter == null)
+            {
+                return null;
+            }
+            bool usevalue = (bool)value;
+
+            //usevalue: true代表某radiobutton按下
+            //parameter: 代表哪個radiobutton按下 
+            if (usevalue)
+            {
+                return parameter.ToString();
+            }
+
+            return null;
+        }
+    }
+}
